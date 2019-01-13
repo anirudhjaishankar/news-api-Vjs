72a70084f48feb3057b9a2ad69d6185776c62879
Anirudh Jaishankar <anirudhjaishankar97@gmail.com>
2019-01-13 19:17:11 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var url = 'https://newsapi.org/v2/everything?q=cricket&apiKey=9e32519c216c44fdbd894c162382cdb6';
+var req = new Request(url);
+$(document).ready(function(){
+  let a;
+  fetch(req)
+      .then(function(response) {
+          a = response.json();
+          a.then(function(obj){
+            // console.log(obj.articles[0]);
+            for(var i=0;i<20;i++){
+                $("#box").append("<div class='box'><div class='columns'><div class='column'><img src=" + obj.articles[i].urlToImage +"></div><div class='column'><h1 class='title is-3'>" + obj.articles[i].title + "</h1><p>" + obj.articles[i].description+ "</p><a href='" + obj.articles[i].url + "'></a></div></div></div>");
+                // console.log(obj['articles'][i]);
+              }
+            });
+      })
+})
